6cc552156b3142736cd5a81e72d365f60e4ca24a
nhan2211 <220057xa@yse-c.net>
2023-12-12 11:18:41 +0900

    update html

---
+++ server.js
@@ -0,0 +1,39 @@
+// express module 読み込み
+const express = require('express')
+// http の createServer module 読み込み
+const { createServer } = require('node:http');
+// Socket.io の Sever module 読み込み
+const { Server } = require('socket.io');
+
+// Expressサーバー作成
+const app = express();
+// サーバの設定
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public'));
+
+const server = createServer(app);
+
+// Socket.io を作成
+const io = new Server(server);
+
+// .envの読み込み
+const dotenv = require('dotenv');
+dotenv.config();
+const host = process.env.HOST
+const port = process.env.PORT
+
+//Socket.ioの接続
+io.on('connection', (socket) => {
+    console.log('connected!!!')
+
+    //chatメッセージの受信
+    socket.on('chat_message', (data) => {
+        console.log(data);
+    })
+})
+
+
+//サーバ起動
+server.listen(port, host, () => {
+    console.log(`listening on http://${host}:${port}`);
+})
